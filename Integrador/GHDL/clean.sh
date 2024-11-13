c101eee05a2d587294e3db95d161071b3816d44a
elpatosilva <elpatosilva@gmail.com>
2024-11-13 18:35:05 -0300

    wip

--- Integrador/GHDL/clean.sh
+++ Integrador/GHDL/clean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-test -e work-obj93.cf && rm work-obj93.cf
+test -e work-obj93.cf && rm -v work-obj93.cf
 
 for file in $(ls *.vhd) ; do
    name=$(basename -s .vhd "$file")
    lname=$(echo ${name} | tr '[:upper:]' '[:lower:]')
    for i in "$lname" "e~${lname}.o" "${name}.o" "${name}.vcd" ; do
-      test -e "$i" && rm "$i"
+      test -e "$i" && rm -v "$i"
    done
 done
